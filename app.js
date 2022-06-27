0066003a319f1391632b52854356e0a42d71c40f
hossein <hossein838485@gmail.com>
2022-06-27 14:31:31 +0430

    add apple and ..

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
 let $ = document;
 let startBtn = $.querySelector(".start");
-let score = $.querySelector(".score");
+let scores = $.querySelector(".score");
 let squares = $.querySelectorAll("div");
-let snakeCurent = [1,2,3]
-let appleCurent = null
-let way = +1
-let squareWidth =10
-let map = document.querySelector('.map')
+let snakeCurent = [3, 2, 1];
+let appleCurent = null;
+let way = +1;
+let squareWidth = 10;
+let map = document.querySelector(".map");
+let score = 0;
+scores.innerHTML = score;
 // startgame
 
 function indexSnake() {
-  let x = null
+  let x = null;
   for (let i = 0; i < snakeCurent.length; i++) {
-    x = snakeCurent[i]
-    squares[x].classList.add('snake');   
+    x = snakeCurent[i];
+    squares[x].classList.add("snake");
   }
 }
 function startGame() {
-  startBtn.style.display = 'none'
-  snakeCurent = [3,2,1]
-  appleCurent = null
-  way = +1
-  indexSnake()
-  moveSquares()
+  startBtn.style.display = "none";
+  snakeCurent = [3, 2, 1];
+  appleCurent = null;
+  way = +1;
+  indexSnake();
+  moveSquares();
+  createApple();
 }
 function moveSquares() {
   setInterval(() => {
-    let firstSquare = snakeCurent.pop()
-    squares[firstSquare].classList.remove('snake')
-    snakeCurent.unshift(snakeCurent[0] + way)
+    let headSnake = snakeCurent[0];
+    let firstSquare = snakeCurent.pop();
+    squares[firstSquare].classList.remove("snake");
+    snakeCurent.unshift(headSnake + way);
     console.log(snakeCurent);
-    indexSnake()
-    if (snakeCurent[0] % 10 == 0 || snakeCurent[0] < 0) {
-      map.style.display = 'none'
-      
+    indexSnake();
+
+    if (headSnake == appleCurent) {
+      score += 1;
+      scores.innerHTML = score;
+      squares[appleCurent].classList.remove("apple");
+      snakeCurent.push(headSnake + way);
+      createApple();
     }
+
   }, 1000);
 }
+
 function moveSnake(e) {
-  
   switch (e.code) {
     case "KeyD":
-      way = +1;
-      moveSquares()
+      way = 1;
+      moveSquares();
       break;
     case "KeyS":
-      way = +squareWidth;
+      way = squareWidth;
       break;
     case "KeyA":
       way = -1;
@@ -54,6 +63,11 @@ function moveSnake(e) {
       way = -squareWidth;
       break;
   }
+}
+
+function createApple() {
+  appleCurent = Math.floor(Math.random() * 100);
+  squares[appleCurent].classList.add("apple");
 }
 window.addEventListener("keyup", moveSnake);
 startBtn.addEventListener("click", startGame);
