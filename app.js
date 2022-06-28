e1dab832ac80d2645652dea083d9382fcb40a848
hossein <hossein838485@gmail.com>
2022-06-28 15:44:09 +0430

    add limit as snake

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ let squareWidth = 10;
 let map = document.querySelector(".map");
 let score = 0;
 scores.innerHTML = score;
+let x = null;
+
 // startgame
 
 function indexSnake() {
-  let x = null;
   for (let i = 0; i < snakeCurent.length; i++) {
     x = snakeCurent[i];
     squares[x].classList.add("snake");
@@ -28,7 +29,7 @@ function startGame() {
   createApple();
 }
 function moveSquares() {
-  setInterval(() => {
+  let movePS = setInterval(() => {
     let headSnake = snakeCurent[0];
     let firstSquare = snakeCurent.pop();
     squares[firstSquare].classList.remove("snake");
@@ -44,6 +45,20 @@ function moveSquares() {
       createApple();
     }
 
+    let ofsetLeftSquares = squares[10].offsetLeft - 5;
+    console.log(ofsetLeftSquares);
+
+    if (snakeCurent[0] + 10 >= 100) {
+      clearInterval(movePS);
+    } else if (snakeCurent[0] % 10 == 0) {
+      clearInterval(movePS);
+    } else if ((snakeCurent[0] - 1) % 10 == 0) {
+      clearInterval(movePS);
+    } else if (snakeCurent[0] - 10 < 0 && way === -10) {
+      clearInterval(movePS);
+    } else if (squares[snakeCurent[0] + 1].classList.contains('snake')){
+      clearInterval(movePS);
+    }
   }, 1000);
 }
 
@@ -51,7 +66,6 @@ function moveSnake(e) {
   switch (e.code) {
     case "KeyD":
       way = 1;
-      moveSquares();
       break;
     case "KeyS":
       way = squareWidth;
